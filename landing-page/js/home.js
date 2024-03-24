283256f05fcfd2aa54de8fc2e95a486f1de9976c
Michael Vandi <mikevee2013@gmail.com>
2024-03-24 09:08:14 -0700

    Added update style to allow clicking

--- landing-page/js/home.js
+++ landing-page/js/home.js
@@ -2,31 +2,63 @@
  * Javascript methods for Home page on Landing page
  */
 
+window.addEventListener("load", function () {
+
+    triggerBottomRequestAccessProcedure();
+
+});
+
 /**
  * Request Access Procedure
  */
 const REQUEST_ACCESS_API_ENDPOINT = "";
-// Fetching DOM elements
-const requestAccessButtons = document.querySelectorAll(".addy-request-access-button");
-// For all request access buttons, apply the on click listener to it
-if (requestAccessButtons && requestAccessButtons.length) {
-    requestAccessButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-            // What happens when the button is clicked
 
-        });
-    });
-} else {
-    // TODO: Log this error, and alert team if needed
+
+function updateStylesForButtomRequestAccessView(input, button) {
+
+    if (!(input && button)) return;
+    // Both input and button exist
+    // Get height of input button
+    const inputHeight = input.offsetHeight;
+    // Add styles to the input
+    input.style.position = "absolute";
+    input.style.zIndex = "999";
+    input.style.color = "rgba(255, 255, 255, 1)";
+
+    // Add styles to the button
+    button.style.position = "absolute";
+    button.style.zIndex = "9999";
+    button.style.border = "1px solid transparent";
+    button.style.right = "5px";
+    // Make button height 84% of the input height
+    button.style.height = `${inputHeight * 0.84}px`;
+
 }
 
-// The bottom sections request Access
-const bottomRequestAccessSection = document.querySelector(".addy-request-access-bottom-div");
-if (bottomRequestAccessSection) {
-    // Find the form input
-    bottomSectionRequestAccessProcedure(bottomRequestAccessSection);
-} else {
-    // TODO: Log this error, and alert team if needed
+function triggerBottomRequestAccessProcedure() {
+    // Fetching DOM elements
+    const requestAccessButtons = document.querySelectorAll(".addy-request-access-button");
+    // For all request access buttons, apply the on click listener to it
+    if (requestAccessButtons && requestAccessButtons.length) {
+        requestAccessButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                // What happens when the button is clicked
+
+            });
+        });
+    } else {
+        // TODO: Log this error, and alert team if needed
+    }
+
+    // The bottom sections request Access
+    const bottomRequestAccessSection = document.querySelector(".addy-request-access-bottom-div");
+    if (bottomRequestAccessSection) {
+        // Find the form input
+        bottomSectionRequestAccessProcedure(bottomRequestAccessSection);
+    } else {
+        // TODO: Log this error, and alert team if needed
+    }
+
 }
 
 
@@ -36,33 +68,35 @@ function bottomSectionRequestAccessProcedure(bottomSection) {
         if (!requestAccessForm) throw new Error("RequestAccessForIsUndefined");
         // Form exists, find the input element and button
         const emailInput = requestAccessForm.querySelector('input[type="email"][name="email"]');
-        if(!emailInput) throw new Error("EmailInputIsUndefined");
+        if (!emailInput) throw new Error("EmailInputIsUndefined");
 
         // Find the submit input element
         const submitButton = requestAccessForm.querySelector('input[type="submit"]');
-        if(!submitButton) throw new Error("SubmitButtonIsUndefined");
+        if (!submitButton) throw new Error("SubmitButtonIsUndefined");
 
         // Both email input and submit button exist
+        // Update styles for the input and button
+        updateStylesForButtomRequestAccessView(emailInput, submitButton);
 
         // Add Click Event Listener to the Submit Button
-        submitButton.addEventListener('click', function(event) {
+        submitButton.addEventListener('click', function (event) {
             event.preventDefault();
             onRequestAccessPressed(emailInput.value);
         });
 
         // Add enter key Event Listener to the Email Input Field
-        emailInput.addEventListener('keydown', function(event) {
+        emailInput.addEventListener('keydown', function (event) {
             if (event.keyCode === 13) { // Check if 'Enter' was pressed
                 event.preventDefault();
                 onRequestAccessPressed(emailInput.value);
             }
         });
-        
+
 
     } catch (error) {
         // TODO: Log this error or alert the team if needed
     }
-    
+
 }
 
 /**
@@ -99,27 +133,27 @@ async function callRequestAccessAPI(email) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({"email": email})
+        body: JSON.stringify({ "email": email })
     })
-    .then(response => response.json())
-    .then(data => {
-        const message = data.message;
-        if (data.success) {
-            console.log(message);
-            return {
-                "success": true,
-                "message": message,
+        .then(response => response.json())
+        .then(data => {
+            const message = data.message;
+            if (data.success) {
+                console.log(message);
+                return {
+                    "success": true,
+                    "message": message,
+                }
+            } else {
+                throw new Error(message);
             }
-        } else {
-            throw new Error(message);
-        }
-    }).catch((error) => {
-        console.error('Error:', error);
-        return {
-            "success": false,
-            "message": error,
-        };
-    });
+        }).catch((error) => {
+            console.error('Error:', error);
+            return {
+                "success": false,
+                "message": error,
+            };
+        });
 }
 
 /**
