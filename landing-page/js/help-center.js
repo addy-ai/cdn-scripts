1896c36d08cee8e336421809bde01061d3a73aad
Michael Vandi <mikevee2013@gmail.com>
2024-04-23 04:05:52 -0700

    Added rouite for how to train and help-center

---
+++ landing-page/js/help-center.js
@@ -0,0 +1,12 @@
+function redirectToUtilsRoute() {
+    // Construct new url
+    var newUrl = "https://addyai.notion.site/Addy-Help-Center-85fc9228fa344dfca8cd6b11ff8bc34f";
+    // Redirect to new url
+    window.location.href = newUrl;
+}
+
+// DOM content loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // redirect to utils route
+    redirectToUtilsRoute();
+});
---
+++ landing-page/js/how-to-train.js
@@ -0,0 +1,12 @@
+function redirectToUtilsRoute() {
+    // Construct new url
+    var newUrl = "https://addyai.notion.site/How-to-Train-Addy-AI-ae438a34c38144ddae4f516ee7714ce2";
+    // Redirect to new url
+    window.location.href = newUrl;
+}
+
+// DOM content loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // redirect to utils route
+    redirectToUtilsRoute();
+});
