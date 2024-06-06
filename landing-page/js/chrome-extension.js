e15225b42d33b450684c415db2462b7c695d0cd7
Michael Vandi <mikevee2013@gmail.com>
2024-06-06 11:14:32 -0700

    Added method to report conversions to google ads

---
+++ landing-page/js/chrome-extension.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+
+    // Report conversions to Google Ads
+    reportConversionsToGoogleAds();
+
+});
+
+function reportConversionsToGoogleAds() {
+    const chromeExtensionURL = "https://chromewebstore.google.com/detail/addy-ai-chatgpt-email-ass/gldadickgmgciakdljkcpbdepehlilfn";
+    const downloadExtensionButtons = document.querySelectorAll(`a[href="${chromeExtensionURL}"]`);
+
+    downloadExtensionButtons.forEach(function (button) {
+        button.addEventListener("click", () => {
+            gtag_report_conversion();
+        });
+    });
+}
+
