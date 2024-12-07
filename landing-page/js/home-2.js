2c7fc1ad9b3d328ebdd4590b3f69b015b68913ef
Michael Vandi <mikevee2013@gmail.com>
2024-12-07 14:35:25 -0500

    Added handler to play testimonial video

--- landing-page/js/home-2.js
+++ landing-page/js/home-2.js
@@ -24,3 +24,42 @@ function reportConversionsToGoogleAds() {
         });
     });
 }
+
+
+function handleTestimonialVideo() {
+    const playButton = document.getElementById("home-testimonial-play-button");
+    const thumbnail = document.getElementById("home-testimonial-thumbnail");
+
+    if (playButton) {
+        playButton.addEventListener("click", togglePlayTestimonialVideo)
+    }
+
+    if (thumbnail) {
+        thumbnail.addEventListener("click", togglePlayTestimonialVideo)
+    }
+}
+
+function togglePlayTestimonialVideo(playButton, thumbnail) {
+    // hide the two elements
+    playButton.style.display = "none";
+    thumbnail.style.display = "none";
+
+    var style = document.createElement('style');
+
+    // Add CSS rules for hiding the play button and thumbnail
+    style.innerHTML = '#home-testimonial-play-button, #home-testimonial-thumbnail { display: none; }';
+    document.head.appendChild(style);
+
+    // get the video element
+    var video = document.querySelector("#home-testimonial-video video");
+
+    // enable the controls and start playing the video
+    if (video) {
+      video.controls = true;
+      video.muted = false;
+      video.play();
+    } else {
+      console.warn("Could not find a video element in #home-testimonial-video");
+    }
+}
+
